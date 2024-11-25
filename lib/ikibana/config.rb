784ac871cc784664129ec9afa1700436470afb6c
Aram <aramhrptn@hotmail.com>
2024-11-25 07:17:31 +0400

    Refactor config loading with YAML and ERB integration

--- lib/ikibana/config.rb
+++ lib/ikibana/config.rb
@@ -1,14 +1,32 @@
 # frozen_string_literal: true
 
+require 'yaml'
+require 'erb'
+
 module Ikibana
   class Config
-    # def initialize
-    #   yield self if block_given?
-    # end
+    attr_reader :config
 
-    def self.configure
-      instance = new
+    def initialize(config_file = "config/nats.yaml")
+      @config = load_config(config_file)
+    end
+
+    def self.configure(config_file = "config/nats.yaml")
+      instance = new(config_file)
       yield instance if block_given?
+    end
+
+    private
+
+    def load_config(file)
+      if File.exist?(file)
+        erb = ERB.new(File.read(file))
+        YAML.safe_load(erb.result, aliases: true)
+      else
+        raise "Configuration file #{file} not found"
+      end
+    rescue Psych::SyntaxError => e
+      raise "YAML syntax error occurred while parsing #{file}: #{e.message}"
     end
   end
 end
