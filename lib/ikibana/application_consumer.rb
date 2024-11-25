61f00005b2111c0c70879fad2e8cf6c025464cc1
Aram <aramhrptn@hotmail.com>
2024-11-25 09:02:08 +0400

    Introduce ApplicationConsumer with mandatory perform method
    This commit introduces the ApplicationConsumer class with a template method pattern, requiring subclasses to implement the `perform` method. Additionally, the worker generator has been renamed to consumer generator to reflect the updated class structure. These changes standardize the way consumers are implemented and improve code maintainability.

--- lib/ikibana/application_consumer.rb
+++ lib/ikibana/application_consumer.rb
@@ -2,5 +2,22 @@
 
 module Ikibana
   class ApplicationConsumer
+    def self.call(...)
+      new(...).call
+    end
+
+    def initialize(...)
+
+    end
+
+    def call
+      perform
+    end
+
+    private
+
+    def perform
+      raise NotImplementedError, "Subclasses must implement a `perform` method"
+    end
   end
 end
