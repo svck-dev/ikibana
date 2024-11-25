29cbcbf33ff8bc1a22b78f96a14955ae663a099b
Aram <aramhrptn@hotmail.com>
2024-11-25 09:00:00 +0400

    Rename worker templates to consumer templates
    Updated the generator to use consumer naming conventions instead of worker. Both the implementation and spec templates have been renamed accordingly for consistency. This ensures clearer intent and consistency across the codebase.

--- lib/generators/ikibana/worker/worker_generator.rb
+++ lib/generators/ikibana/worker/worker_generator.rb
@@ -5,11 +5,11 @@ module Ikibana
     source_root File.expand_path("../../templates", __dir__)
 
     def create_worker
-      template "worker.rb.erb", File.join("app/ikibana", class_path, "#{file_name}_consumer.rb")
+      template "consumer.rb.erb", File.join("app/ikibana", class_path, "#{file_name}_consumer.rb")
     end
 
     def create_worker_spec
-      template "worker_spec.rb.erb", File.join("spec/ikibana", class_path, "#{file_name}_consumer_spec.rb")
+      template "consumer_spec.rb.erb", File.join("spec/ikibana", class_path, "#{file_name}_consumer_spec.rb")
     end
   end
 end
