565b86027ae16742ed62c0ad3886ee0b9d0b791c
Aram <aramhrptn@hotmail.com>
2024-11-25 07:04:23 +0400

    Add InstallGenerator for Ikibana with NATS configuration template

---
+++ lib/generators/ikibana/install/install_generator.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Ikibana
+  module Generators
+    # This class is responsible for creating the initial files in your app folder
+    class InstallGenerator < Rails::Generators::Base
+      source_root File.expand_path("../../templates", __dir__)
+
+      def create_directories
+        template "nats.yaml.erb", "config/nats.yaml"
+      end
+    end
+  end
+end
